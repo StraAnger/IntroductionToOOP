a42a6f73b1400d26611d8e3e97428763ac98517f
StraAnger <Stranger.01@mail.ru>
2021-11-21 22:07:49 +0400

    Struct

--- Introduction to OOP/main.cpp
+++ Introduction to OOP/main.cpp
@@ -2,7 +2,7 @@
 
 #define tab "\t"
 
-struct point {
+struct Point {
 
 	double x;
 	double y;
@@ -10,12 +10,17 @@ struct point {
 };
 
 int main() {
-	
-	point A;
+	//type name; объ€вление переменной ј типа структуры/класса Point
+	//или объ€вление объекта типа Point ( структуры Point), или же создание экземпл€ра ј структуры Point 
+	Point A;
 	A.x = 2;
 	A.y = 3;
 
 	std::cout << A.x<<tab<<A.y<<std::endl;
+
+	Point* pA = &A; //объ€вл€ем указатель на Point 'pA', и инициализируем его адресом объекта 'A'
+
+	std::cout << pA->x << tab << pA->y << std::endl;
 
 	return 0;
 }
