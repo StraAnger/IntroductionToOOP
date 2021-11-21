2edb5dcd7d5e10fdb4743321f2eecb58be8cc204
StraAnger <Stranger.01@mail.ru>
2021-11-21 21:41:11 +0400

    v 1.0

--- Introduction to OOP/main.cpp
+++ Introduction to OOP/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 
+#define tab "\t"
+
+struct point {
+
+	double x;
+	double y;
+
+};
 
 int main() {
 	
+	point A;
+	A.x = 2;
+	A.y = 3;
 
-
-
-
-
+	std::cout << A.x<<tab<<A.y<<std::endl;
 
 	return 0;
 }
