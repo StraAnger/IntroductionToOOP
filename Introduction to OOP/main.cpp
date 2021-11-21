77b33b950934ece177893eb79082e23ef93590f8
StraAnger <Stranger.01@mail.ru>
2021-11-21 23:22:07 +0400

    Class

--- Introduction to OOP/main.cpp
+++ Introduction to OOP/main.cpp
@@ -1,15 +1,47 @@
 #include <iostream>
 
+//#define STRUCT_PINT
+
 #define tab "\t"
 
+/*
+
 struct Point {
+	double x;
+	double y;
+};
+
+*/
+
+class Point {
 
 	double x;
 	double y;
+public:
+
+	double get_x() const {
+		return x;
+	}
+
+	double get_y() const {
+		return y;
+	}
+
+	void set_x(double x) {
+		this->x = x;
+	}
+
+	void set_y(double y) {
+		this->y = y;
+	}
 
 };
 
+
+
 int main() {
+
+#ifdef STRUCT_POINT
 	//type name; объ€вление переменной ј типа структуры/класса Point
 	//или объ€вление объекта типа Point ( структуры Point), или же создание экземпл€ра ј структуры Point 
 	Point A;
@@ -21,6 +53,15 @@ int main() {
 	Point* pA = &A; //объ€вл€ем указатель на Point 'pA', и инициализируем его адресом объекта 'A'
 
 	std::cout << pA->x << tab << pA->y << std::endl;
+#endif //STRUCT_POINT
+
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+
+
+	std::cout << A.get_x() << tab << A.get_y() << std::endl;
+
 
 	return 0;
 }
